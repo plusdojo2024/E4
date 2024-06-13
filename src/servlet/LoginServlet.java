59ee6862c7408b4b5a0f08028e843ce85b4fb993
plusdojo2024-konno <konno-yuka-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-13 18:28:17 +0900

    Eclipseファイル追加

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class LoginServlet
  */
-@WebServlet("/IndexServlet")
-public class IndexServlet extends HttpServlet {
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public IndexServlet() {
+    public LoginServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
