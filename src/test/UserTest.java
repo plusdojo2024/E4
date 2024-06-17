484790c9da34cf44d09bbdc703ee647261ba1f33
plusdojo2024-sasaki <sasaki-naoki-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-17 14:15:16 +0900

    テストクラス作成

---
+++ src/test/UserTest.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+	@Test
+	void loginTest() {
+		private boolean result = false;
+		fail("まだ実装されていません");
+	}
+
+}
