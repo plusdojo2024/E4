e9148d558d0ce17618c6cc9039dbb653aa43f7cc
plusdojo2024-okazaki <okazaki-kazushi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-18 13:34:49 +0900

    DAO更新

--- src/dao/EventDAO.java
+++ src/dao/EventDAO.java
@@ -63,7 +63,7 @@ public class EventDAO {
 		return result;
 	}
 
-     //ユーザーIDからイベント（未参加）を表示
+     //ユーザーIDからイベント（参加）を表示
 	public List<Event> searchuserId(int userId) {
 		Connection conn = null;
 		List<Event> cardList = new ArrayList<Event>();
@@ -121,10 +121,10 @@ public class EventDAO {
 		return cardList;
 	}
 
-	//ユーザーIDからイベント（参加）を表示  途中
-			public List<Event> fetchParticipant(int userId) {
+	//ユーザーIDからイベント（未参加）を表示
+			public Event fetchParticipant(int eventId) {
 				Connection conn = null;
-				List<Event> participantList = new ArrayList<Event>();
+				Event event = null;
 
 				try {
 					// JDBCドライバを読み込む
@@ -134,7 +134,59 @@ public class EventDAO {
 					conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/IGNITE", "sa", "");
 
 					// SQL文を準備する
-					String sql = "INSERT INTO ";
+					String sql = "SELECT * FROM event WHERE id = ?";
+
+					PreparedStatement pStmt = conn.prepareStatement(sql);
+					pStmt.setString(1,"eventId");
+
+					ResultSet rs = pStmt.executeQuery();
+					rs.next();
+					// 結果表をコレクションにコピーする
+					    event = new Event(
+					    rs.getInt("id"),
+					    rs.getString("event_name"),
+					    rs.getString("event_description"),
+						rs.getString("holding_schedule"),
+						rs.getInt("least_count"),
+						rs.getInt("max_count"),
+						rs.getInt("prefectureId"),
+						rs.getString("detail_address"),
+						rs.getString("location_name"),
+						rs.getInt("event_category"),
+						rs.getInt("holdingUserId"),
+						rs.getInt("status")
+					  );
+				}catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					// データベースを切断
+					if (conn != null) {
+						try {
+							conn.close();
+						}
+						catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+
+				// 結果を返す
+				return event;
+			}
+
+			public List<Event> searchHoldingEvent(int userId) {
+				Connection conn = null;
+				List<Event> cardList = new ArrayList<Event>();
+
+				try {
+					// JDBCドライバを読み込む
+					Class.forName("org.h2.Driver");
+
+					// データベースに接続する
+					conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/IGNITE", "sa", "");
+
+					// SQL文を準備する
+					String sql = "SELECT * FROM event WHERE holding_user_id = ?";
 
 					PreparedStatement pStmt = conn.prepareStatement(sql);
 					pStmt.setString(1,"userId");
@@ -157,7 +209,7 @@ public class EventDAO {
 						rs.getInt("holdingUserId"),
 						rs.getInt("status")
 					  );
-					  participantList.add(record);
+					    cardList.add(record);
 								}
 				}catch (Exception e) {
 					e.printStackTrace();
@@ -174,7 +226,7 @@ public class EventDAO {
 				}
 
 				// 結果を返す
-				return participantList;
+				return cardList;
 			}
 
 
