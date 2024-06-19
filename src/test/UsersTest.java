bc7209079e862080cfdb6b8b1bc8ec2857ae6e60
plusdojo2024-okazaki <okazaki-kazushi-plusdojo2024@seplus2016.onmicrosoft.com>
2024-06-19 15:27:47 +0900

    テスト更新

--- src/test/UsersTest.java
+++ src/test/UsersTest.java
@@ -52,9 +52,20 @@ public class UsersTest {
 		assertEquals(expected, actual);
 	}*/
 
-	//insert
+	//searchuserId
 	@Test
 	void ユーザーID検索成功() throws Exception {
+	  String mailAddress = "user1@example.com";
+
+	  String actual = userDao.searchuserId(mailAddress); //実際の実行結果
+	  String expected = "1";
+
+	  assertEquals(expected, actual); //実際の実行結果と期待する実行結果が合っているか
+	}
+
+	//insert
+	@Test
+	void プロフィール保存成功() throws Exception {
 	  Users users = new Users(null,"user1@example.com","Passw0rd!","山田 太郎","1985-05-15","090-1234-5678",0,1,0,10,"2024-09-25 00:00:00",81,11,31,51,4,0,1) ;
 
 	  boolean actual = userDao.insert(users);
@@ -62,5 +73,64 @@ public class UsersTest {
 
 	  assertEquals(expected, actual); //実際の実行結果と期待する実行結果が合っているか
 	}
+
+	//insertSearchPrefecture
+	@Test
+	void 参加可能範囲の保存成功() throws Exception {
+	  int userId = "1";
+	  ArrayList<Integer> prefectures = "1";//不安
+
+	  boolean actual = userDao.insertSearchPrefecture(userId,prefectures);
+	  boolean expected = true;
+
+	  assertEquals(expected, actual); //実際の実行結果と期待する実行結果が合っているか
+	}
+
+	//update
+	@Test
+	void ユーザーのデータの更新成功() throws Exception {
+	    Users users = new Users(null,"user1@example.com","Passw0rd!","山田 太郎","1985-05-15",0,1,0,10,"2024-09-25 00:00:00",81,11,31,51,4,0,1) ;
+	    String teNum = "user01@example.com";
+	    int prefectureid = 2;
+	    int eventCategory = 1;
+	    ArrayList<Integer> prefectures = "1";
+
+	    boolean actual = userDao.update(users,telNum,prefectureid,eventCategory);
+	    boolean expected = true;
+	}
+
+	//reviewParamUpdate
+	@Test
+	void 評価の更新成功() throws Exception {
+	    Users users = new Users(null,"user1@example.com","Passw0rd!","山田 太郎","1985-05-15",0,1,0,10,"2024-09-25 00:00:00",81,11,31,51,4,0,1) ;
+	    int evluation = 82;
+	    int technicParam = 22;
+	    int cookParam = 51;
+	    int communicationParam = 81;
+	    int targetUserId = 1
+
+	    boolean actual = userDao.reviewParamUpdate(users,evluation,technicParam,cookParam,communicationParam,targetUserId);
+	    boolean expected = true;
+	}
+
+	//setIconUpdate
+	void アイコンの更新成功() throws Exception {
+	    Users users = new Users(null,"user1@example.com","Passw0rd!","山田 太郎","1985-05-15",0,1,0,10,"2024-09-25 00:00:00",81,11,31,51,4,0,1) ;
+	    int iconId = 2;
+
+	    boolean actual = userDao.setIconUpdate(users.iconId);
+	    boolean expected = true;
+	}
+
+	//fetchAchievements
+	//searchIcon
+	@Test
+	void アイコンの取得成功() throws Exception {
+	  int userId = 1;
+
+	  String actual = userDao.searchIcon(userId);
+	  String expected = "1";
+	}
+	//fetchUser
 
 }
